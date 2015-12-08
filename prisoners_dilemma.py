7b89e36c45062cfdb1c87f567a1aa4b922b3339c
storesund98 <stevenstoresund@gmail.com>
2015-12-08 08:37:41 -0800

    my new code
    no time, class over

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -527,19 +527,19 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #
     elif player == 17:
         if getting_team_name:
-            return 'loyal vengeful'
+            return 'analizer'
         else:
             if len(opponent_history)==0: #It's the first round: collude
                 return 'c'
-            elif history[-1]=='c' and opponent_history[-1]=='b':
-                return 'b' # betray is they were severely punished last time
+            elif opponent_history[-1]=='b' and opponent_history[-2]=='b' and opponent_history[-3]=='b':
+                return 'b'
+            elif opponent_history[-1]=='c' and opponent_history[-2]=='b' and opponent_history[-3]=='c':
+                return 'b'
+            elif opponent_history[-1]=='c' and opponent_history[-2]=='c' and opponent_history[-3]=='c':
+                return 'b'
             else:
-                return 'c' #otherwise collude
+                return 'c'
     
-    
-
-
-
 
 
 
